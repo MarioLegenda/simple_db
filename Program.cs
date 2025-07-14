0756298ab94eab157077e23f4884f5763609cc70
mario <marioskrlec222@proton.me>
2025-07-14 13:53:29 +0200

    Added entry to create the database file if it does not exist

--- Program.cs
+++ Program.cs
@@ -1,38 +1,22 @@
-﻿//
-//using database.Database;
-//
-// string simulatedDsn = "/home/mario/applications/database/output.bin";
-//
-// Driver driver = new Driver(simulatedDsn);
-//     
-// IWriter writer = driver.GetWriter();
-//
-// writer.Write("some value that is shit");
-
-using System.Net.Sockets;
+﻿using System.Net.Sockets;
 using database.Server;
 
-class Program
-{
-    static void Main()
-    {
-        TcpListener? listener = null;
-        try
-        {
-            Server server = new Server(5000);
-            server.Start();
+TcpListener? listener = null;
 
-            listener = server.GetServer();
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Error: " + e);
-            listener?.Stop();
-        }
-        finally
-        {
-            listener?.Stop();
-        }
-    }
+try
+{
+    Server server = new Server(5000);
+    server.Start();
+
+    listener = server.GetServer();
+}
+catch (Exception e)
+{
+    Console.WriteLine("Error: " + e);
+    listener?.Stop();
+}
+finally
+{
+    listener?.Stop();
 }
 
